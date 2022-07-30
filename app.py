ad477fb630224c1361213330a82dc55ffe4e6ab3
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-07-30 19:11:47 +0200

    feat: add calc_in_rust function

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from shiny import App, render, ui
 
-from pyshiny_rust_lib import hello_rust
+from pyshiny_rust_lib import hello_rust, calc_in_rust
 
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
-    ui.output_text_verbatim("hello")
+    ui.output_text_verbatim("hello"),
+    ui.output_text_verbatim("calc")
 )
 
 
@@ -20,6 +21,11 @@ def server(input, output, session):
     @render.text
     def hello():
         return hello_rust()
+
+    @output
+    @render.text
+    def calc():
+        return f"n*2+2={calc_in_rust(input.n())}"
 
 
 app = App(app_ui, server)
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,14 @@ fn hello_rust() -> PyResult<String> {
     Ok("Hello from Rust!".to_string())
 }
 
+#[pyfunction]
+fn calc_in_rust(x: i32) -> PyResult<i32> {
+    Ok(x * 2 + 2)
+}
+
 #[pymodule]
 fn pyshiny_rust_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_rust, m)?)?;
+    m.add_function(wrap_pyfunction!(calc_in_rust, m)?)?;
     Ok(())
 }
