7aaae2c14089dfe17547225b08ee519dbe929764
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-07-30 19:07:37 +0200

    feat: add Rust - pyshiny interop

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from shiny import App, render, ui
 
+from pyshiny_rust_lib import hello_rust
+
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
+    ui.output_text_verbatim("hello")
 )
 
 
@@ -12,6 +15,11 @@ def server(input, output, session):
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
+
+    @output
+    @render.text
+    def hello():
+        return hello_rust()
 
 
 app = App(app_ui, server)
